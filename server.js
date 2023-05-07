dba834982e04d4ef3e438271b55ac4152fc64220
Itay Segev <itaysegev123@gmail.com>
2023-05-08 00:21:11 +0300

    added cors

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 import express from "express";
 import data from "./data.js";
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 
 // End points 
 app.get('/api/v1/products',(req,res) => {
-
     res.status(200).json(data.products);
-
 });
 
 app.listen(PORT, () => {
     console.log(`the server is running on port ${PORT}`);
-
 })
