55287cc28df72d317f70a18b161eb1e1f014e7b0
Itay Segev <itaysegev123@gmail.com>
2023-05-11 17:17:51 +0300

    Added a new Route

--- data.js
+++ data.js
@@ -1,43 +1,42 @@
 const data = {
-    products:[
-        {
-            name: 'productA-name',
-            category: '',
-            description:'',
-            price: 100,
-            token:'productA-token',
-            brand: 'Stam',
-            countInStock: 10,
-            rating: 2.5,
-            numReviews:250,
-            image: '/images/A.jpg'
-        },
-        {
-            name: 'productB-name',
-            category: '',
-            description:'',
-            price: 20,
-            token:'productB-token',
-            brand: 'Brandy',
-            countInStock: 12,
-            rating: 3,
-            numReviews:200,
-            image: '/images/B.jpg'
-        },
-        {
-            name: 'productC-name',
-            category: '',
-            description:'',
-            price: 50,
-            token:'productC-token',
-            brand: 'Stam',
-            countInStock: 10,
-            rating: 2.5,
-            numReviews:80,
-            image: '/images/C.jpg'
-        },
-        
-    ]
-}
+  products: [
+    {
+      name: 'productA-name',
+      description: 'stam',
+      price: 100,
+      image: '/images/A.jpg',
+      category: 'stam',
+      countInStock: 10,
+      token: 'productA-token',
+      brand: 'Stam',
+      rating: 2.5,
+      numReviews: 250,
+    },
+    {
+      name: 'productB-name',
+      description: 'stam',
+      price: 100,
+      image: '/images/B.jpg',
+      category: 'stam',
+      countInStock: 20,
+      token: 'productB-token',
+      brand: 'Stam',
+      rating: 2,
+      numReviews: 250,
+    },
+    {
+      name: 'productC-name',
+      description: 'stam',
+      price: 100,
+      image: '/images/C.jpg',
+      category: 'stam',
+      countInStock: 19,
+      token: 'productC-token',
+      brand: 'Stam',
+      rating: 4.6,
+      numReviews: 29,
+    },
+  ],
+};
 
 export default data;
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-import express from "express";
-import data from "./data.js";
+import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import data from './data.js';
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+const PORT = process.env.PORT || 4040;
 const app = express();
+
+// middleware
 
 app.use(cors());
 
-// End points 
-app.get('/api/v1/products',(req,res) => {
-    res.status(200).json(data.products);
+//Endpoints
+
+app.use('/api/v1/seed', seedRouter);
+
+app.get('/api/v1/products', (req, res) => {
+  res.send(data.products);
 });
 
-app.listen(PORT, () => {
-    console.log(`the server is running on port ${PORT}`);
-})
+mongoose
+  .connect(process.env.MONGOO_CONNECT)
+  .then(() => {
+    console.log('\nconnected to Mongo succefully :)\n');
+    app.listen(PORT, () => {
+      console.log(`Server is listening on PORT : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Failed to connect to Mongo: ${error.message}`);
+  });
