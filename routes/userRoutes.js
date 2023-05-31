e95dfde16ce4b7ddf8b23c4b2aa5da4b2cd79c2e
Itay Segev <itaysegev123@gmail.com>
2023-06-01 01:05:57 +0300

    fixed a bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,9 @@ import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
       if (bcrypt.compareSync(req.body.password, user.password)) {
@@ -20,6 +22,26 @@ userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
       }
     }
     res.status(401).send({ message: 'Invalid password/email' });
+  })
+);
+
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+
+    const user = await newUser.save();
+
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user),
+    });
   })
 );
 
